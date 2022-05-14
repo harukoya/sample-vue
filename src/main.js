6739f413e1e6974142e1b4abf2c7b64832fcc29d
harukoya <ka.uni.stu@gmail.com>
2022-05-14 15:18:32 +0900

    カスタムディレクティブのフック関数

--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import LikeNumber from './components/LikeNumber.vue'
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber)
+Vue.directive("border", {
+  bind() {
+    // ディレクティブが初めて対象の要素に紐づいたとき
+  },
+  inserted() {
+    // 親Nodeに挿入されたとき
+  },
+  update() {
+    // コンポーネントが更新されるたび。子コンポーネントが更新される前
+  },
+  componentUpdated() {
+    // コンポーネントが更新されるたび。子コンポーネントが更新された後
+  },
+  unbind() {
+    // ディレクティブが紐づいている要素から取り除かれた時
+  }
+});
 
 new Vue({
   render: h => h(App),
