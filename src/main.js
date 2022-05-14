0a30c1f88980a89b2a790113d33ade9eae90782b
harukoya <ka.uni.stu@gmail.com>
2022-05-14 15:25:42 +0900

    フック関数はbindとupdateがほぼ使うので、vue.jsが省略記法を使えるようにしている

--- src/main.js
+++ src/main.js
@@ -4,22 +4,8 @@ import LikeNumber from './components/LikeNumber.vue'
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber)
-Vue.directive("border", {
-  bind() {
-    // ディレクティブが初めて対象の要素に紐づいたとき
-  },
-  inserted() {
-    // 親Nodeに挿入されたとき
-  },
-  update() {
-    // コンポーネントが更新されるたび。子コンポーネントが更新される前
-  },
-  componentUpdated() {
-    // コンポーネントが更新されるたび。子コンポーネントが更新された後
-  },
-  unbind() {
-    // ディレクティブが紐づいている要素から取り除かれた時
-  }
+Vue.directive("border", function (el, binding) {
+
 });
 
 new Vue({
