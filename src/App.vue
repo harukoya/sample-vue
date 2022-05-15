22a22f650660d6dd53b77a1378d8b7d6919ae8d6
harukoya <ka.uni.stu@gmail.com>
2022-05-15 09:28:51 +0900

    同じタグならば、要素のみを差し替える処理をvueがするので、アニメーションが効かない key属性を設定して、タグが異なることを知らせればアニメーションが効くようになる

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
     <button @click="myAnimation = 'fade'">Fade</button>
     <p>現在設定されているアニメーション：{{ myAnimation }}</p>
     <button v-on:click="show = !show">切り替え</button>
+    <transition name="fade">
+      <p v-if="show" key='bye'>さようなら</p>
+      <p v-else key='hello'>こんにちは</p>
+    </transition>
     <transition
       enter-active-class="animate__animated animate__bounce"
       leave-active-class="animate__animated animate__rubberBand"
